091de0d212bfc8a6c3b7706015a72b594824c968
rufetisr <rufet.isr123@gmail.com>
2022-12-21 12:49:36 -0800

    prepared List Page and links

--- myapp/src/Components/List/list.jsx
+++ myapp/src/Components/List/list.jsx
@@ -6,10 +6,11 @@ import { useRef } from 'react';
 
 
 const list = ({ obj, setObj, arr, setArr }) => {
-    let [isActive, setActive] = useState("true");
+    let [isActive, setActive] = useState(true);
     let [value, setValue] = useState();
-
+    let [inputActive, setInputActive] = useState(false);
     let listInput = useRef();
+
     let textChange = (e) => {
         e.target.value != "" ? setActive(false) : setActive(true);
     }
@@ -17,8 +18,10 @@ const list = ({ obj, setObj, arr, setArr }) => {
     let goToList = () => {
         if (listInput.current.value.length != 0) {
             console.log(listInput.current.value);
+            setValue(listInput.current.value);
+            setInputActive(true);
         }
-        setValue(listInput.current.value.length);
+
     }
 
     let Delete = (item) => {
@@ -27,18 +30,18 @@ const list = ({ obj, setObj, arr, setArr }) => {
         obj.splice(index, 1);
         setObj([...obj]);
         arr.splice(index, 1)
-        setArr([...arr])
+        setArr(arr)
         console.log(obj);
         console.log(arr);
     }
 
     return (
         <div className='list'>
-            <input ref={listInput} onChange={textChange} type="text" placeholder='List adini daxil edin' />
+            <input ref={listInput} onChange={textChange} type="text" placeholder='List adini daxil edin' disabled={inputActive} />
             <ul className='favourite-list' >
                 {
                     obj.map((item) => {
-                        return (                            
+                        return (
                             <div style={{ display: "flex" }}>
                                 {
                                     item.Title ? <li key={item.Title}> {item.Title} ({item.Year})</li>
@@ -52,12 +55,10 @@ const list = ({ obj, setObj, arr, setArr }) => {
                             </div>
                         )
                     })
-
                 }
             </ul>
             {
-
-                value > 0 ? <Link to="/List">Go to</Link> : <button onClick={goToList} disabled={isActive}>Save list</button>
+                value  ? <Link to="/List" state={obj} >Go to</Link> : <button onClick={goToList} disabled={isActive}>Save list</button>
             }
         </div>
     )
--- myapp/src/Pages/List.jsx
+++ myapp/src/Pages/List.jsx
@@ -1,12 +1,27 @@
 import React from 'react';
-
+import { useLocation } from 'react-router-dom';
+import "./List.css"
 const List = () => {
+
+  const stateParamValue = useLocation().state;
+
+  let url = "https://m.imdb.com/title/";
+  // const location = useLocation();
+  // const obj = location.state;
+  // const { type }= useParams();
+  // console.log(stateParamValue);
   return (
-    <div>
-      <h2>My list</h2>
-        <ul>
-            
-        </ul>
+    <div className='links'>
+      <h1>My list </h1>
+      <ul>
+        {
+          stateParamValue.map((item) => {
+            return (
+             <a href={url+`${item.imdbID}`}> <li>{item.Title} ({item.Year})</li></a>
+            )
+          })
+        }
+      </ul>
     </div>
   );
 }
